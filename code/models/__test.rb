f645e158027c3898ee562aba12a7a3d2f1cd6966
Steven Kay <stevendkay@gmail.com>
2018-11-10 14:30:50 +0000

    added vendor, fixed deletions

--- code/models/__test.rb
+++ code/models/__test.rb
@@ -1,8 +1,11 @@
 require_relative('./account')
+require_relative('./vendor')
+
 
 # this is a quick test to make sure my
 # changes to SqlRunner actually work
 
+# accounts table
 account = Account.new({"name"=>"Second Account"})
 account.save
 account.name = "Second Account, updated!"
@@ -12,4 +15,14 @@ Account.all.each do |record|
 end
 account2 = Account.find(account.id)
 p account2
-account2.delete()
+account2.delete
+
+# vendors table
+vendor = Vendor.new({"name"=>"Test Vendor"})
+vendor.save
+vendor.name = "Test vendor, updated"
+vendor.update
+Vendor.all.each do |record|
+  p record
+end
+vendor.delete
---
+++ code/models/vendor.rb
@@ -0,0 +1,44 @@
+require_relative('../db/sql_runner')
+
+class Vendor
+
+  attr_reader :id
+  attr_accessor :name
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+  end
+
+  def save
+    sql = '
+      INSERT INTO vendors ("name") VALUES ($1) RETURNING *;
+    '
+    params = [@name]
+    @id = SqlRunner.create(sql,params)
+  end
+
+  def self.all
+    sql = "SELECT * from vendors;"
+    return SqlRunner.all(sql,Vendor)
+  end
+
+  def self.find(id)
+    sql = "SELECT * from vendors WHERE id=$1;"
+    params=[id]
+    return SqlRunner.get(sql, params, Vendor)
+  end
+
+  def update
+    sql = "UPDATE vendors SET name = $1 WHERE id=$2 RETURNING *;"
+    params = [@name,@id]
+    SqlRunner.run(sql,params)
+  end
+
+  def delete
+    sql = "DELETE from vendors WHERE id=$1;"
+    params = [@id]
+    SqlRunner.run(sql,params)
+  end
+
+end
