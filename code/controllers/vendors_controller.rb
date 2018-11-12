82864546deefee17912707f79b532859cc49238d
Steven Kay <stevendkay@gmail.com>
2018-11-12 17:11:12 +0000

    added new vendor form, additional notes

--- code/controllers/vendors_controller.rb
+++ code/controllers/vendors_controller.rb
@@ -8,3 +8,15 @@ get '/vendors' do
   @vendors = Vendor.all
   erb(:vendors)
 end
+
+get '/vendors/new' do
+  # show form for new vendor creation
+  erb(:vendor)
+end
+
+post '/vendors' do
+  # create and save a new Account
+  vendor = Vendor.new(params)
+  vendor.save
+  redirect to '/'
+end
